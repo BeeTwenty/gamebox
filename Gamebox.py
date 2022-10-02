4867c54b28ad557f036f163170a539fcec52f2a6
sindre berge <sindrebrergee96@gmail.com>
2022-10-02 06:49:08 +0200

    added Readme.md file

--- Gamebox.py
+++ Gamebox.py
@@ -1,9 +1,6 @@
-
-
-
 games = ["madlibs"]
 def game_select():
-    
+
     print("let's open a game:")
     print("games you can select are:")
     print(games)
@@ -22,10 +19,10 @@ def madlibs ():
     verb1 = input("Give me 3 verbs, verb 1: ")
     verb2 = input("verb 2: ")
     verb3 = input("verb 3: ")
-    
+
     print("It was %s day at school, and %s was super %s for lunch. But when she went outside to eat, a %s stole her %s! %s chased the %s all over school. She %s, %s, and %s through the playground. Then she tripped on her %s and the %s escaped! Luckily, %s’s friends were willing to share their %s with her." % (food, name, adj, noun, food, name, noun, verb1, verb2, verb3, noun, noun, name, food))
     game_select()
-     
+
 print("Hello Welcome!")
 
 print("Who are you?")
